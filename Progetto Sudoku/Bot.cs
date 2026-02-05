9ea644900cff440957479e2bc734df91e020c785
Alessio Zarbo <alessio.zarbo@itsincom.it>
2026-02-05 17:00:26 +0100

    Implementazione metodo ControlloBlocchi

--- Progetto Sudoku/Bot.cs
+++ Progetto Sudoku/Bot.cs
@@ -18,6 +18,94 @@ namespace Progetto_Sudoku {
         public void ControlloColonne() { }
 
         // Alessio
-        public void ControlloBlocchi() { }
+        public bool ControlloBlocchi(int[,] matrice, int riga, int col) {
+
+            int[] coordinateCol = new int[3];
+            int[] coordinateRiga = new int[3];
+
+            // Controllo se le coordinate siano sbagliate
+            if (riga < 0 || riga > 8 || riga < 0 || riga > 8) 
+                return false;
+
+            // Controllo per individuare in quale blocco è la cordinata riga
+            if (riga < 3) { 
+            
+                // La riga è nel primo blocco
+                coordinateRiga[0] = 0;
+                coordinateRiga[1] = 1;
+                coordinateRiga[2] = 2;
+            } else if (riga >= 3 && riga < 6) {
+
+                // La riga è nel secondo blocco
+                coordinateRiga[0] = 3;
+                coordinateRiga[1] = 4;
+                coordinateRiga[2] = 5;
+            } else if (riga >=6) {
+
+                // La riga è nel terzo blocco
+                coordinateRiga[0] = 6;
+                coordinateRiga[1] = 7;
+                coordinateRiga[2] = 8;
+            }
+
+            // Controll per individuare in quale blocco è la cordinata col
+            if (col < 3) {
+                
+                // La colonna è nel primo blocco
+                coordinateCol[0] = 0;
+                coordinateCol[1] = 1;
+                coordinateCol[2] = 2;
+            } else if (col >= 3 && col < 6) {
+
+                // La colonna è nel secondo blocco
+                coordinateCol[3] = 3;
+                coordinateCol[4] = 4;
+                coordinateCol[5] = 5;
+            } else if (col >= 6) {
+
+                // La colonna è nel terzo blocco
+                coordinateCol[6] = 6;
+                coordinateCol[7] = 7;
+                coordinateCol[8] = 8;
+            }
+
+            int[,] bloccoTemporaneo =
+            {
+                // Creazione riga 1
+                {
+                    matrice[coordinateRiga[0], coordinateCol[0]],
+                    matrice[coordinateRiga[0], coordinateCol[1]],
+                    matrice[coordinateRiga[0], coordinateCol[2]]
+                },
+                // Creazione Riga 2
+                {
+                    matrice[coordinateRiga[1], coordinateCol[0]],
+                    matrice[coordinateRiga[1], coordinateCol[1]],
+                    matrice[coordinateRiga[1], coordinateCol[2]]
+                },
+                // Creazione Riga 3
+                {
+                    matrice[coordinateRiga[2], coordinateCol[0]],
+                    matrice[coordinateRiga[2], coordinateCol[1]],
+                    matrice[coordinateRiga[2], coordinateCol[2]]
+                }
+            };
+
+            int valoreBot = matrice[riga, col];
+
+            // Controllo vero e proprio
+            for (int y = 0; y < 4; y += 1) {
+
+                for (int x = 0; x < 4; x += 1) {
+
+                    // se vengono trovati due numeri uguali OLTRE al numero nella stessa posizione, vuoldire che ci sono due duplicati, restituirà false
+                    if (bloccoTemporaneo[y, x] == valoreBot /*&& y != riga && x != col*/)
+                        return false;
+                }
+            }
+
+            // Nel caso non entra nella condizione del false, restituirà che nel blocco è giusto
+            return true;
+        }
     }
 }
