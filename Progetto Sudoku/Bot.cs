7b910c5924f4b84f18f9bb9d3d3980664a98f04b
Marco Michele Accorinti <marco.accorinti@itsincom.it>
2026-02-05 17:40:12 +0100

    Prima versione del Bot

--- Progetto Sudoku/Bot.cs
+++ Progetto Sudoku/Bot.cs
@@ -1,17 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Progetto_Sudoku {
     internal class Bot {
 
         public static void Main(string[] args) {
 
-            int[,] matriceModifiiabile = Matrice.matrice;
+            int[,] matriceModificabile = Matrice.matrice;
+            
         }
 
         // Marco
-        public void Avvio() { }
+        public void Avvio(int n, int riga, int col) 
+        {
+            int[,] matriceModificabile = Matrice.matrice;
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    for (int numero = 1; numero < 9; numero++) 
+                    {
+                        if (Controllo && matriceModificabile[riga, col] == Matrice.matrice[riga,col] && matriceModificabile[riga,col] == 0)
+                        {
+                            matriceModificabile[riga, col] = n;
+                        }
+                        Console.Write(matriceModificabile?[i, j] + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
 
         // Shehan
         public void ControlloRighe() { }
