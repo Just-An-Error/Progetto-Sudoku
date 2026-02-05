1eaa909c39ebe081f5acc6fbd04da2162a34b39f
Alessio Zarbo <alessio.zarbo@itsincom.it>
2026-02-05 14:58:26 +0100

    Modifica di readonly alla classe Matrice

--- Progetto Sudoku/Matrice.cs
+++ Progetto Sudoku/Matrice.cs
@@ -6,7 +6,7 @@ namespace Progetto_Sudoku
 {
     internal static class Matrice
     {
-        public static int[,] matrice =
+        private static readonly int[,] matrice =
         {
             {5 , 3, 0, 0, 7, 0, 0, 0, 0 },
             {6, 0, 0, 1, 9, 5, 0, 0, 0 },
