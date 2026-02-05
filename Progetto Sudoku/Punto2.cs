fda92604e84a78e4e2577fc68d7b6d51927d174d
Alessio Zarbo <alessio.zarbo@itsincom.it>
2026-02-05 15:30:50 +0100

    Modifica per iniziare il Punto 2
    - Ho modificato la visibilita a 'public' della variabile statica
    - Ho implementato il metodo 'Main' All'interno della classe Punto2

--- Progetto Sudoku/Punto2.cs
+++ Progetto Sudoku/Punto2.cs
@@ -4,5 +4,10 @@ using System.Text;
 
 namespace Progetto_Sudoku {
     internal class Punto2 {
+
+        public static void Main(string[] args) {
+
+            int[,] matriceModifiiabile = Matrice.matrice;
+        }
     }
 }
