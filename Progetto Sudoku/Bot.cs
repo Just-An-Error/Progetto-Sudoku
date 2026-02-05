a4d9815f7fc973279d894892a1556b592eabe92b
Alessio Zarbo <alessio.zarbo@itsincom.it>
2026-02-05 15:49:05 +0100

    Cambio nome classe, implementazione dei metodi vuoti

---
+++ Progetto Sudoku/Bot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Progetto_Sudoku {
+    internal class Bot {
+
+        public static void Main(string[] args) {
+
+            int[,] matriceModifiiabile = Matrice.matrice;
+        }
+
+        // Marco
+        public void Avvio() { }
+
+        // Shehan
+        public void ControlloRighe() { }
+        public void ControlloColonne() { }
+
+        // Alessio
+        public void ControlloBlocchi() { }
+    }
+}
